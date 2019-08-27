821f73b333e0feb3f18601012adb329921c487e5
github MedKim <stock.realestate.fx@gmail.com>
2019-08-27 17:36:12 +0900

    nestingコンポーネント

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
+import Ninjas from './ninja';
 
-class app extends React.Component {
+
+class App extends React.Component {
   render(){
   return (
     <div className="App">
+      <h1>First App</h1>
+      {/* Ninjaコンポーネント */}
+      <Ninjas />
     </div>
   );
   }
