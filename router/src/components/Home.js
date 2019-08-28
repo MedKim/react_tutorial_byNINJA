a9889437e142766e3a75d7a06f73c21e5890f21f
github MedKim <stock.realestate.fx@gmail.com>
2019-08-28 22:56:01 +0900

    react-routerでルーティング

---
+++ router/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () =>{
+   return(
+      <div className="container">
+         <h4 className="center">Home</h4>
+         <p>sample message paragrahs</p>
+      </div>
+   )
+};
+
+export default Home;
