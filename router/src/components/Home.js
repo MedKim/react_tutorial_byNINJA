0a3b66ad5f3d024442eb99d941f11fe11fc5c057
github MedKim <stock.realestate.fx@gmail.com>
2019-08-29 15:46:36 +0900

    画像のimport

--- router/src/components/Home.js
+++ router/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Pokeball from '../pokeball.png';
 
 class Home extends Component {
    state = {
@@ -24,6 +25,7 @@ class Home extends Component {
          posts.map(post => {
             return (
                <div className="post card" key={post.id}>
+                  <img src={Pokeball} alt="A Poke ball" />
                   <div className="card-content">
                      <Link to={'/' + post.id}>
                         <span className="card-title">{post.title}</span>
@@ -37,7 +39,7 @@ class Home extends Component {
          <div className="center">No Posts Yet</div>
       )
       return(
-         <div className="container">
+         <div className="container home">
             <h4 className="center">Home</h4>
             {postList}
          </div>
